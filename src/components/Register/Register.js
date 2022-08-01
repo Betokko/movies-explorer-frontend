5d33057efd3775bf2d493f1059826a38de6bc953
Artur Nikitin <branewor@gmail.com>
2022-08-01 23:22:21 +0400

    add input validation

--- src/components/Register/Register.js
+++ src/components/Register/Register.js
@@ -1,28 +1,99 @@
-import React from "react";
-import { Link } from "react-router-dom";
-import Input from "../Input/Input";
+import React, { useEffect, useState } from 'react';
+import { Link } from 'react-router-dom';
+import { useForm } from 'react-hook-form';
+import validator from 'validator';
 
-import "./Register.scss";
+import './Register.scss';
 
 const Register = () => {
+  const {
+    register,
+    handleSubmit,
+    reset,
+    formState: { errors, isValid },
+  } = useForm({ mode: 'onChange' });
+
+  const onSubmit = (data) => {
+    console.log(data);
+    reset();
+  };
+
   return (
     <section className="register">
       <div className="register__container">
         <Link to="/" className="header__logo register__logo" />
-        <form>
+        <form onSubmit={handleSubmit(onSubmit)}>
           <h2 className="register__title">Добро пожаловать!</h2>
-          <Input type="text" name="name" placeholder="Имя" />
-          <Input type="email" name="email" placeholder="Почта" />
-          <Input type="password" name="password" placeholder="Пароль" />
-          <input
+
+          <label className="text-field__label" htmlFor="name">
+            Имя
+            <input
+              {...register('name', {
+                required: 'Введите имя',
+                minLength: {
+                  value: 2,
+                  message: 'Имя должно содержать не менее 2 символов',
+                },
+              })}
+              className="text-field__input"
+              type="text"
+              id="name"
+            />
+            {errors.name && (
+              <div className="text-field__error">{errors.name.message}</div>
+            )}
+          </label>
+
+          <label className="text-field__label" htmlFor="email">
+            Email
+            <input
+              {...register('email', {
+                required: 'Введите email',
+                validate: {
+                  checkEmail: (v) =>
+                    validator.isEmail(v) || 'Укажите корректный email',
+                },
+              })}
+              className="text-field__input"
+              type="email"
+              id="email"
+            />
+            {errors.email && (
+              <div className="text-field__error">{errors.email.message}</div>
+            )}
+          </label>
+          <label className="text-field__label" htmlFor="password">
+            Пароль
+            <input
+              {...register('password', {
+                required: 'Введите пароль',
+                minLength: {
+                  value: 2,
+                  message: 'Пароль должен содержать не менее 2 символов',
+                },
+              })}
+              className="text-field__input"
+              type="password"
+              id="password"
+            />
+            {errors.password && (
+              <div className="text-field__error">{errors.password.message}</div>
+            )}
+          </label>
+
+          <button
+            className="text-field__submit"
             type="submit"
-            value="Зарегистрироваться"
-            className="register__submit"
-          />
+            disabled={!isValid}
+          >
+            Зарегистрироваться
+          </button>
         </form>
         <div className="register__bottom">
           <div>Уже зарегистрированы?</div>
-          <Link className="register__bottom__link" Link to="/">Войти</Link>
+          <Link className="register__bottom__link" to="/">
+            Войти
+          </Link>
         </div>
       </div>
     </section>
