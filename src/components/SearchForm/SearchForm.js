a081134e8d67ce3f77da368616813e7621a99bf9
Artur Nikitin <branewor@gmail.com>
2022-08-04 19:12:51 +0400

    feat: add the error message at SearchForm

--- src/components/SearchForm/SearchForm.js
+++ src/components/SearchForm/SearchForm.js
@@ -1,29 +1,41 @@
 import React from 'react';
 
+import { useForm } from 'react-hook-form';
+
 import './SearchForm.scss';
 
 const SearchForm = () => {
+  const {
+    register,
+    formState: { errors },
+    handleSubmit,
+    reset,
+  } = useForm();
+
+  const onSubmit = (data) => { reset() }
+
   return (
     <section className="search-form">
-      <form className="search-form__container">
+      <form className="search-form__container" onSubmit={handleSubmit(onSubmit)}>
         <input
           type="text"
           placeholder="Фильм"
           className="search-form__container__field"
-          required={true}
+          {...register('search', { required: 'Нужно ввести ключевое слово' })}
         />
         <button
           type="submit"
           value=""
           className="search-form__container__button"
         />
+      <div className="search-form__container__error">{errors.search && errors.search.message}</div>
       </form>
       <div className="search-form__shorts">
         <span className="search-form__shorts__title">Короткометражки</span>
         <label htmlFor="shorts" className="search-form__shorts__checkbox">
           <input type="checkbox" id="shorts" />
-          <span className='search-form__shorts__checkbox__bg'></span>
-          <i className='search-form__shorts__checkbox__indicator'></i>
+          <span className="search-form__shorts__checkbox__bg"></span>
+          <i className="search-form__shorts__checkbox__indicator"></i>
         </label>
       </div>
     </section>
